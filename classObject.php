74ea4fd994ba72ad3f0151bee48807749bf23436
Asya-Meira <63652487+Asya-Meira@users.noreply.github.com>
2020-08-20 23:34:19 -0700

    file utama

---
+++ classObject.php
@@ -0,0 +1,20 @@
+<? php
+
+	//contoh class
+	class sabun{
+		
+
+		// contoh dari properti
+		public $berbusa;
+		public $licin;
+
+		// contoh dari method
+		function membersihkan{
+			return "bluk bluk bluk":
+		}
+	}
+
+	//contoh membuat object dari class "sabun"
+	echo $jazz1 = new sabun ();
+
+?>
